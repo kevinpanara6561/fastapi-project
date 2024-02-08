a84ec5d2b0109d420b1c24d7f5073a8e264c0a86
kevinpanara6561 <kevinpanara6561@gmail.com>
2024-02-08 12:58:56 +0530

    api pass added

--- app/config.py
+++ app/config.py
@@ -3,23 +3,23 @@ import os
 
 from fastapi import HTTPException, status
 
-# DB_HOST = os.environ.get("PRO_DB_HOST")
-# DB_USER = os.environ.get("PRO_DB_USER")
-# DB_PASSWORD = os.environ.get("PRO_DB_PASSWORD")
-# DB_NAME = os.environ.get("PRO_DB_NAME")
-# JWT_KEY = os.environ.get("PRO_JWT_KEY")
-# RE_CAPTCHA_SECRET = os.environ.get("PRO_RE_CAPTCHA_SECRET")
-# SES_FROM_EMAIL = os.environ.get("PRO_SES_FROM_EMAIL")
-# BUCKET_NAME = os.environ.get("PRO_BUCKET_NAME")
-
-DB_HOST = "localhost"
-DB_USER = "admin"
-DB_PASSWORD = "admin"
-DB_NAME = "projectx"
-JWT_KEY = '{"k":"xuQvsl68aTATKRhHjBYo1msJckIJqAj9PooXELeojpY","kty":"oct"}'
+DB_HOST = os.environ.get("PRO_DB_HOST")
+DB_USER = os.environ.get("PRO_DB_HOST")
+DB_PASSWORD = os.environ.get("PRO_DB_PASSWORD")
+DB_NAME = os.environ.get("PRO_DB_NAME")
+JWT_KEY = os.environ.get("PRO_JWT_KEY")
 RE_CAPTCHA_SECRET = os.environ.get("PRO_RE_CAPTCHA_SECRET")
 SES_FROM_EMAIL = os.environ.get("PRO_SES_FROM_EMAIL")
 BUCKET_NAME = os.environ.get("PRO_BUCKET_NAME")
+
+# DB_HOST = "localhost"
+# DB_USER = "admin"
+# DB_PASSWORD = "admin"
+# DB_NAME = "projectx"
+# JWT_KEY = '{"k":"xuQvsl68aTATKRhHjBYo1msJckIJqAj9PooXELeojpY","kty":"oct"}'
+# RE_CAPTCHA_SECRET = os.environ.get("PRO_RE_CAPTCHA_SECRET")
+# SES_FROM_EMAIL = os.environ.get("PRO_SES_FROM_EMAIL")
+# BUCKET_NAME = os.environ.get("PRO_BUCKET_NAME")
 
 if JWT_KEY:
     try:
